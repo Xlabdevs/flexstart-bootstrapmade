804366fddc9ed7e42ad935564ac753b9da234f82
Xlabdevs <88905239+Xlabdevs@users.noreply.github.com>
2021-08-15 03:30:46 -0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -286,12 +286,5 @@
     aos_init();
   });
   
-  // Basic example
-$(document).ready(function () {
-$('#dtBasicExample').DataTable({
-"searching": true // false to disable search (or any other option)
-});
-$('.dataTables_length').addClass('bs-select');
-});
 
 })();
